fd435f03483ee552167a3e859918ca69f233c631
leo <leo@localhost.localdomain>
2019-05-13 07:55:13 -0400

    fix bug in ngRoute

---
+++ public/js/main.js
@@ -0,0 +1,45 @@
+var app=angular.module('aloha',["ngRoute"]);
+app.config(['$routeProvider',function ($routeProvider) {
+    $routeProvider
+    .when("/",{
+        templateUrl:"../../pages/home.htm",
+        controller:"ProductController"
+    })
+    .when("cart/",{
+        templateUrl: "../../pages/cart.htm",
+        controller:"CartController"
+    })
+    .otherwise({
+        redirectTo:"/"
+    });
+}
+]);
+var cartList=[];
+
+app.controller('ProductController',['$scope',function($scope){
+    debugger;
+    $scope.products=[
+        {
+        "image":"public/iphone.jpg",
+        "name":"IPhone 10x",
+        "price":73999.00
+        },
+        {
+        "image":"public/samsung.jpg",
+        "name":"Samsung 21",
+        "price":54000.00
+        },
+        {
+        "image":"public/nokia21.jpg",
+        "name":"Nokia 21",
+        "price":43000.00
+        }
+    ];
+    $scope.addToCart=function(product){
+        cartList.push(product);
+        console.log(cartList);
+    };
+}]);
+app.controller('CartController',['$scope',function($scope){
+    $scope.cart=cartList;
+}]);
